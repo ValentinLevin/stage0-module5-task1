862bdae191600a3eb1be19cf69688634c4e4bdc2
Valentin Levin <levin_valentin@mail.ru>
2023-12-09 12:42:44 +0600

    task completed

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -11,7 +11,7 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-
+        return new String[]{"winter", "spring", "summer", "autumn"};
     }
 
     /**
@@ -25,7 +25,11 @@ public class ArrayTasks {
      * length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = i+1;
+        }
+        return result;
     }
 
     /**
@@ -37,7 +41,12 @@ public class ArrayTasks {
      * arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-
+        int result = 0;
+        int length = arr.length;
+        for (int i=0; i < length; i++) {
+            result += arr[i];
+        }
+        return result;
     }
 
     /**
@@ -50,7 +59,13 @@ public class ArrayTasks {
      * arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
-
+        int length = arr.length;
+        for (int i = 0; i < length; i++) {
+            if (number == arr[i]) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     /**
@@ -63,7 +78,12 @@ public class ArrayTasks {
      * arr = ["pineapple", "apple", "pen"] -> ["pen", "apple", "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
-
+        int length = arr.length;
+        String[] result = new String[length];
+        for (int i = 0; i < length; i++) {
+            result[length-i-1] = arr[i];
+        }
+        return result;
     }
 
     /**
@@ -78,7 +98,18 @@ public class ArrayTasks {
      * arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
-
+        int length = arr.length;
+        int[] result = new int[length];
+        int positiveNumberCount = 0;
+        for (int i = 0; i<length; i++) {
+            if (arr[i] > 0) {
+                positiveNumberCount++;
+                result[positiveNumberCount-1] = arr[i];
+            }
+        }
+        int[] positiveNumbers = new int[positiveNumberCount];
+        System.arraycopy(result, 0, positiveNumbers, 0, positiveNumberCount);
+        return positiveNumbers;
     }
 
     /**
@@ -91,7 +122,51 @@ public class ArrayTasks {
      * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]]
      * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
-    public int[][] sortRaggedArray(int[][] arr) {
 
+    public int[][] sortRaggedArray(int[][] arr) {
+        int arrRowCount = arr.length;
+        int arrMinItemCount = arr[0].length;
+        int arrMaxItemCount = 0;
+
+        for (int i = 0; i < arrRowCount; i++) {
+            if (arrMaxItemCount < arr[i].length) {
+                arrMaxItemCount = arr[i].length;
+            }
+            if (arrMinItemCount > arr[i].length) {
+                arrMinItemCount = arr[i].length;
+            }
+        }
+
+        int[][] result = new int[arrRowCount][];
+
+        int searchedItemCount = arrMinItemCount;
+        int resultRowCount = 0;
+        while (searchedItemCount <= arrMaxItemCount) {
+            for (int arrIndex = 0; arrIndex < arrRowCount; arrIndex++) {
+                if (arr[arrIndex].length == searchedItemCount) {
+                    resultRowCount++;
+                    result[resultRowCount-1] = sort(arr[arrIndex]);
+                }
+            }
+            searchedItemCount++;
+        }
+
+        return result;
+    }
+
+    public int[] sort(int[] arr) {
+        int length = arr.length;
+        int[] result = new int[length];
+        System.arraycopy(arr, 0, result, 0, length);
+        for (int i = 0; i < length; i++) {
+            for (int j = 1; j < length - i; j++) {
+                if (arr[j-1] > arr[j]) {
+                    int buf = arr[j];
+                    result[j] = arr[j-1];
+                    result[j-1] = buf;
+                }
+            }
+        }
+        return result;
     }
 }
